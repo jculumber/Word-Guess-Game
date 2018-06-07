77722ced519c1c6ac4f6247940933f474db83fa4
jculumber <jennr2@aol.com>
2018-06-07 00:12:02 -0400

    Fixed game glitches and added Media Queries

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,55 @@
 //Values at the beginning of a new game
 var wins = 0;               //Number of times the player has correctly guessed the word
-var currentWord;            //Current game word trying to be guessed
+var currentWord = "";            //Current game word trying to be guessed
 var remainingGuesses = 20;  //Number of chances the player has to figure out word
 var lettersGuessed = [];    //Empty array for guessed letters
 var guessingWord = [];      //Combination of "_" and letters player has guessed to build word
 var playerGuess;            //Key pressed by player
-var currentWordLetters;     //Letters in currentWord
+var currentWordLetters = [];     //Letters in currentWord
 
 //List of possible words to be used for a game
-var gameWords = ["magickingdom", "epcot", "hollywoodstudios", "animalkingdom", "mickeymouse", "donaldduck", "fireworks"]; 
-
-//Begin
-window.onload = alert("Press 'New Game' button to begin.")
+var gameWords = ["magickingdom", "epcot", "hollywoodstudios", "animalkingdom", "mickeymouse", "donaldduck", "fireworks"];
 
 //Start a new game
-    function newGame() {
-        //Display wins
-        document.getElementById("winsTotal").innerHTML = wins; 
-        
-        //Display Remaining Guesses
-        document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
-    
-        //Generate the current word
-        currentWord = gameWords[Math.floor(Math.random() * gameWords.length)];
-        currentWordLetters = [currentWord.split("")];
+function newGame() {
+    lettersGuessed = [];    //Empty array for guessed letters
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
+    guessingWord = [];      //Combination of "_" and letters player has guessed to build word
+    document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+    //Display wins
+    document.getElementById("winsTotal").innerHTML = wins;
 
-        //Represent currentWord using " _ "
-        for (i = 0; i < currentWord.length; i++) {
+    //Display Remaining Guesses
+    document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+
+    //Generate the current word
+    currentWord = gameWords[Math.floor(Math.random() * gameWords.length)];
+    currentWordLetters = [currentWord.split("")];
+
+    //Represent currentWord using " _ "
+    for (i = 0; i < currentWord.length; i++) {
         guessingWord.push(" _ ");
-        };
-        document.getElementById("currentWord").innerText = "";
-        for (j = 0; j < guessingWord.length; j++) {
-        document.getElementById("currentWord").innerText += guessingWord[j];
-        };
     };
- 
-//This function is run whenever the user presses a key
-    document.onkeyup = function(event) {
-    
-        // Determines which key was pressed, and makes any letter lower case
-        playerGuess = event.key.toLowerCase();
+    document.getElementById("currentWord").innerText = "";
+    for (j = 0; j < guessingWord.length; j++) {
+        document.getElementById("currentWord").innerText += guessingWord[j];
+    };
+};
 
-        //Check to see if letter has already been guessed
-        //If yes - do nothing
-    
-        //If no - add letter to guessed letters array
-        if ((remainingGuesses - lettersGuessed.length) > 0 && (lettersGuessed.indexOf(playerGuess) == -1)) {
-            lettersGuessed.push(" " + playerGuess);        
-            document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
-        
+//This function is run whenever the user presses a key
+document.onkeyup = function (event) {
+
+    // Determines which key was pressed, and makes any letter lower case
+    playerGuess = event.key.toLowerCase();
+
+    //Check to see if letter has already been guessed
+    //If yes - do nothing
+
+    //If no - add letter to guessed letters array
+    if ((remainingGuesses - lettersGuessed.length) > 0 && (lettersGuessed.indexOf(playerGuess) == -1)) {
+        lettersGuessed.push(" " + playerGuess);
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
+
         //Check to see if letter is in word
         //If yes - replace dash with letter in the appropriate position
         for (k = 0; k < currentWord.length; k++) {
@@ -56,26 +57,23 @@ window.onload = alert("Press 'New Game' button to begin.")
                 guessingWord[k] = " " + playerGuess + " ";
                 document.getElementById("currentWord").textContent = guessingWord.join("");
             }
-        //If no - decrement Number of Guesses by 1
+            //If no - decrement Number of Guesses by 1
             else {
-                document.getElementById("remainingGuesses").innerHTML = remainingGuesses- lettersGuessed.length;
+                document.getElementById("remainingGuesses").innerHTML = remainingGuesses - lettersGuessed.length;
             }
         };
-        };
-        //When Remaining Guesses = 0, tell player Game Over, and restart game when next key is pressed
-        if ((remainingGuesses - lettersGuessed.length) == 0) {
-            alert("Game Over! Press 'New Game' button to start a new game.");
-            lettersGuessed = [];    //Empty array for guessed letters
-            document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
-            guessingWord = [];      //Combination of "_" and letters player has guessed to build word
-            document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
-            newGame();
-        };
-    
-        //If all letters in the current word are guessed and Remaining Guesses > 1, tell player they win, and restart game when next key is pressed
-        if (guessingWord.indexOf(" _ ") == -1) {
-            alert("You win! Great job! Press 'New Game' button to start a new game.");
-            wins++;
-        };
-
     };
+    //When Remaining Guesses = 0, tell player Game Over, and restart game when next key is pressed
+    if ((remainingGuesses - lettersGuessed.length) === 0) {
+        alert("Game Over! Press 'New Game' button to start a new game.");
+        newGame();
+    };
+
+    //If all letters in the current word are guessed and Remaining Guesses > 1, tell player they win, and restart game when next key is pressed
+    if (guessingWord.indexOf(" _ ") == -1) {
+        document.getElementById("currentWord").innerHTML = guessingWord.join("");
+        alert("You win! Great job! Press 'New Game' button to start a new game.");
+        wins++;
+    };
+
+};
